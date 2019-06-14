e9893f8936abd93c99b7e344162c1daa3f4b1c83
Jake Barrington <51297178+mjakebarrington@users.noreply.github.com>
2019-06-14 15:31:25 -0400

    First revision. First state complete

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -10,9 +13,9 @@ function ask(questionText) {
 // remember the StateMachine lecture
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
 let states = {
-  'roomOne': { canChangeTo: [ 'roomTwo' ] },
-  'roomTwo': { canChangeTo: [ 'roomThree' ] },
-  'roomThree': { canChangeTo: [ 'roomOne' ] }
+  roomOne: { canChangeTo: ["roomTwo"] },
+  roomTwo: { canChangeTo: ["roomThree"] },
+  roomThree: { canChangeTo: ["roomOne"] }
 };
 
 let currentState = "green";
@@ -22,18 +25,43 @@ function enterState(newState) {
   if (validTransitions.includes(newState)) {
     currentState = newState;
   } else {
-    throw 'Invalid state transition attempted - from ' + currentState + ' to ' + newState;
+    throw "Invalid state transition attempted - from " +
+      currentState +
+      " to " +
+      newState;
   }
 }
-
+//Include Johnson State Rugby Easter Egg
 start();
-
 async function start() {
   const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
-  let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+On the door is a handwritten sign. \n`;
+  var answer = await ask(welcomeMessage);
+  var answer = answer.toLowerCase();
+/*
+---------------------------------------------------------------------------------
+*/
+while (answer !== '12345') {
+  if (answer.includes('12345')) {
+    console.log("Welcome to the foyer at burlington code academy.");
+  } else if (answer.includes('read sign')) {
+    console.log("The sign says 'Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345.'");
+    var answer = await ask('');
+  } else if (answer.includes('take')) {
+    console.log("The sign is bolted to the wall, you bonehead! There is nothing to take at this location.");
+    var answer = await ask('');
+  } else {
+    console.log("Im sorry, I cannot " + answer);
+    var answer = await ask('');
+  }
+}
+/*
+----------------------------------------------------------------------------------
+*/
+  //while (answer !== 'go up' || answer == 'go down' || 'go') {
+    
+  //}
   process.exit();
 }
